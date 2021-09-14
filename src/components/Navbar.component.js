b3f2cff24feaf53e7790fac23e90586ba8164b32
MayurBhowad <mayurbhowad1995@gmail.com>
2021-09-14 13:28:31 +0530

    added: navbar li hover

--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -9,9 +9,9 @@ function Navbar() {
             </div>
             <div>
                 <ul className="hidden sm:flex pr--2">
-                    <li className="px-2 cursor-pointer" ><Link to="/" >Home</Link></li>
-                    <li className="px-2 cursor-pointer">Contact</li>
-                    <li className="px-2 cursor-pointer">User</li>
+                    <li className="px-2 cursor-pointer hover:text-green-500" ><Link to="/" >Home</Link></li>
+                    <li className="px-2 cursor-pointer hover:text-green-500">Contact</li>
+                    <li className="px-2 cursor-pointer hover:text-green-500">User</li>
                 </ul>
             </div>
         </div>

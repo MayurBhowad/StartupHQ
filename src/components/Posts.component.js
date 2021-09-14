a9dd9143ef8b5fac998e9f8f832c24a68e9a552b
MayurBhowad <mayurbhowad1995@gmail.com>
2021-09-14 13:12:02 +0530

    adding: pagination

--- src/components/Posts.component.js
+++ src/components/Posts.component.js
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import Pagination from './pagination/Pagination.component';
 import Post from './post/Post'
 
 function Posts() {
     const apiURI = 'https://jsonplaceholder.typicode.com/posts';
+
     const [posts, setPosts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(5);
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const totalPages = posts.length / postsPerPage
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginateFront = () => setCurrentPage(currentPage + 1);
+    const paginateBack = () => setCurrentPage(currentPage - 1);
+
+    console.log(totalPages);
 
     let displayPostsContainer;
-    if (posts.length > 0) {
-        displayPostsContainer = posts.map(post => (<Post key={post.id} data={post} />))
+    if (currentPosts.length > 0) {
+        displayPostsContainer = currentPosts.map(post => (<Post key={post.id} data={post} />))
     } else {
         displayPostsContainer = <h1>No post found</h1>
     }
@@ -23,6 +37,14 @@ function Posts() {
     return (
         <div className="my-2 mx-2 sm:mx-10 md:mx-20 lg:mx-40">
             {displayPostsContainer}
+            <Pagination
+                totalPages={totalPages}
+                postsCount={posts.length}
+                indexOfFirstPost={indexOfFirstPost}
+                indexOfLastPost={indexOfLastPost}
+                paginateFront={paginateFront}
+                paginateBack={paginateBack}
+            />
         </div>
     )
 }
